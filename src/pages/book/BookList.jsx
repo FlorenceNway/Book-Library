fd72bb17ee4a2f108103812628b025ed231d008e
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-09-22 23:58:05 +0100

    init

---
+++ src/pages/book/BookList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../../components/layout/Layout";
+
+const BookList = () => {
+  return (
+    <div>
+      <Layout> BookList </Layout>
+    </div>
+  );
+};
+export default BookList;
