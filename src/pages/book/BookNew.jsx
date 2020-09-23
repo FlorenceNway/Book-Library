8df76a93f2e3284cb17837abeec1a701cf6d1a31
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-09-23 22:36:37 +0100

    create bookNew

---
+++ src/pages/book/BookNew.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Grid, Segment, Header, Form, Button } from 'semantic-ui-react';
+import Layout from '../../components/layout/Layout';
+import { createBook } from '../../service/book/bookAction';
+
+const BookNew = () => {
+  const [name, setName] = useState();
+  const [description, setDescription] = useState();
+  const dispatch = useDispatch();
+  const bookReducer = useSelector((state) => state.bookReducer);
+
+  const { isPending } = bookReducer;
+
+  const onFormSubmit = () => {
+    // console.log({ name, description });
+    dispatch(createBook({ name, description }));
+  };
+
+  return (
+    <div>
+      <Layout>
+        <Grid centered>
+          <Grid.Row>
+            <Grid.Column computer={8}>
+              <Segment padded="very">
+                <Header>Book New</Header>
+                <Form onSubmit={onFormSubmit}>
+                  <Form.Field>
+                    <label> Name</label>
+                    <input
+                      placeholder="Name"
+                      name="name"
+                      onChange={(event) => setName(event.target.value)}
+                    />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Description</label>
+                    <input
+                      placeholder="description.. "
+                      name="description"
+                      onChange={(event) => setDescription(event.target.value)}
+                    />
+                  </Form.Field>
+                  <Button
+                    type="submit"
+                    loading={isPending}
+                    disabled={isPending}
+                  >
+                    Submit
+                  </Button>
+                </Form>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Layout>
+    </div>
+  );
+};
+export default BookNew;
