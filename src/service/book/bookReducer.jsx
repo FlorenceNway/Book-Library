2b498ed55c06f6bed54be89540a5bf095b29e1bc
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-09-29 00:00:30 +0100

    getbook and select book reducer

--- src/service/book/bookReducer.jsx
+++ src/service/book/bookReducer.jsx
@@ -1,9 +1,11 @@
-import { CREATE_BOOK } from './bookActionType';
+import _ from 'lodash';
+import { CREATE_BOOK, GET_BOOKS, SELECT_BOOKS } from './bookActionType';
 
 const initialState = {
   isPending: false,
   error: false,
-  data: {},
+  data: [],
+  filterData: [],
 };
 
 export default function (state = initialState, action) {
@@ -14,6 +16,24 @@ export default function (state = initialState, action) {
         isPending: action.meta.isPending,
         error: action.meta.error,
       };
+    case GET_BOOKS:
+      return {
+        ...state,
+        isPending: action.meta.isPending,
+        error: action.meta.error,
+        data: action.payload,
+        filterData: action.payload,
+      };
+    case SELECT_BOOKS: {
+      const value = action.payload;
+      const filterValue = _.filter(state.filterData, { name: value });
+      console.log('filterValue', filterValue);
+      return {
+        ...state,
+        data: value === 'All' ? state.filterData : filterValue,
+      };
+    }
+
     default:
       return state;
   }
