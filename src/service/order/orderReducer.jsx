35d78da3370a6d6b9485a8b5c0a662916ac6d556
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-10-04 11:35:57 +0100

    order reducer

---
+++ src/service/order/orderReducer.jsx
@@ -0,0 +1,17 @@
+import { ADD_TO_CART } from './orderActionType';
+
+const initialState = {
+  data: [],
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        data: [...state.data, action.payload],
+      };
+    default:
+      return state;
+  }
+}
