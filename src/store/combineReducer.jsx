12ed6611316670a8f295b50e6986cfa2570dc74c
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-10-04 11:36:16 +0100

    combine orderReducer

--- src/store/combineReducer.jsx
+++ src/store/combineReducer.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import bookReducer from '../service/book/bookReducer';
+import orderReducer from '../service/order/orderReducer';
 
 const rootReducer = combineReducers({
   bookReducer,
+  orderReducer,
 });
 
 export default rootReducer;
