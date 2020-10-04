2b514b4e4261e064f8c5808fd454e7a9b81a42d2
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-10-04 11:30:30 +0100

    Menu bar items

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { Menu } from 'semantic-ui-react';
+import { Icon, Menu, Label } from 'semantic-ui-react';
 import styles from './layout.module.scss';
 
 const Layout = ({ children }) => {
   // {children} all components under Layout will be shown, no need to write each
   const history = useHistory();
+  const orders = useSelector((state) => state.orderReducer.data);
   return (
     <div>
       <Menu className={styles.menuLayout} size="huge" fixed="top">
-        <Menu.Item name="editorials" className={styles.menuItem} />
-        <Menu.Item name="reviews" className={styles.menuItem} />
+        <Menu.Item
+          name="Books"
+          className={styles.menuItem}
+          onClick={() => history.push('/')}
+        />
         <Menu.Item
           name="New"
           className={styles.menuItem}
           onClick={() => history.push('/books/new')}
         />
+        <Menu.Item
+          name="New"
+          className={styles.menuItem}
+          onClick={() => history.push('/books/new')}
+        />
+        <Menu.Item name="cart" position="right">
+          <Icon name="cart" color="yellow" />
+          <Label color="teal">{orders.length}</Label>
+        </Menu.Item>
       </Menu>
       <div className={styles.layoutSection}>{children}</div>
     </div>
